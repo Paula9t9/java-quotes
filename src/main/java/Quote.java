6fb0c7277d5b79d9dd232d686c6fcd237c6cf91c
Paula Thomas <paula9t9@gmail.com>
2019-06-06 10:43:40 -0700

    prints a quote

--- src/main/java/Quote.java
+++ src/main/java/Quote.java
@@ -1,22 +1,43 @@
+import com.google.gson.Gson;
+import com.sun.javafx.binding.StringFormatter;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Quote {
+    private String author;
+    private String text;
+
+    private Quote[] quotes;
 
     public String getQuote(){
 
 
         try {
-            Scanner scanner = new Scanner(new File("src/main/resources/recentquotes.json"));
+            BufferedReader bufferedReader = new BufferedReader(
+                    new FileReader("src/main/resources/recentquotes.json"));
+
+            Gson gson = new Gson();
+
+            quotes = gson.fromJson(bufferedReader, Quote[].class);
+
+            return quotes[0].toString();
+
+
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             e.printStackTrace();
-            return "";
+            return "Insanity: doing the same thing over and over again and expecting different results.";
         }
 
-        return "Insanity: doing the same thing over and over again and expecting different results.";
     }
 
-
+    @Override
+    public String toString() {
+        return String.format("%s" +
+                "\n%s", this.text, this.author);
+    }
 }

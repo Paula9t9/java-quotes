dbf083987319a459a760bcff079d7b74ff6d1236
Paula Thomas <paula9t9@gmail.com>
2019-06-11 08:54:58 -0700

    Pulls a quote from the Starwars API

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ This application shows random quotes using Gson
 
 There is currently no front end for this application. 
 
-To get a quote, navigate to the [App](./src/main/java/quotes/App.java) file in your preferred IDE and run. 
+To get a quote, run 
+
+    ./gradlew run
+        
+ from your command line. 
+ 
 
 You should see a random quote print out to your terminal. 
--- src/main/java/quotes/Quote.java
+++ src/main/java/quotes/Quote.java
@@ -3,41 +3,55 @@ package quotes;
 import com.google.gson.Gson;
 import com.sun.javafx.binding.StringFormatter;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Scanner;
 
 public class Quote {
     private String author;
     private String text;
+    private String starWarsQuote;
 
     private Quote[] quotes;
 
+    // Gets a random quote from the swquotes API. If that fails, it will pull a random quote from out file
     public String getQuote(){
 
-
-        try {
-            BufferedReader bufferedReader = new BufferedReader(
-                    new FileReader("src/main/resources/recentquotes.json"));
+        try{
+            // Get starwars quote from starwars API
+            URL url = new URL("http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            BufferedReader internetReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
             Gson gson = new Gson();
+            Quote newQuote = gson.fromJson(internetReader, Quote.class);
+            return newQuote.starWarsQuote;
 
-            quotes = gson.fromJson(bufferedReader, Quote[].class);
+        }catch (IOException e){
+            System.out.println("Error connecting to API");
+            System.out.println(e);
 
-            int randomIndex = (int) (Math.random() * quotes.length);
+            // If we can't get a quote from the API, pull one from the file
+            try {
+                BufferedReader bufferedReader = new BufferedReader(
+                        new FileReader("src/main/resources/recentquotes.json"));
+                Gson gson = new Gson();
+                quotes = gson.fromJson(bufferedReader, Quote[].class);
+                int randomIndex = (int) (Math.random() * quotes.length);
+                return quotes[randomIndex].toString();
 
-            return quotes[randomIndex].toString();
-
-
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-            e.printStackTrace();
-            return "Insanity: doing the same thing over and over again and expecting different results.";
+            } catch (FileNotFoundException e2) {
+                System.out.println("File not found");
+                e2.printStackTrace();
+                //If we can't pull a quote from the file, return this winner
+                return "Insanity: doing the same thing over and over again and expecting different results.";
+            }
         }
 
+
     }
+
 
     @Override
     public String toString() {
